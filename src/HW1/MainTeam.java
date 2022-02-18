bbef0a889e15a443dc33a246f75c90372960be9b
YuriyL <yuriy1305@gmail.com>
2022-02-18 19:53:05 +0300

    Commit1

--- src/HW1/MainTeam.java
+++ src/HW1/MainTeam.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class MainTeam {
+package HW1;
+//   Yuriy Lomtev, 16/02/2022
+//        В итоге должно получиться похожее:
+//      public static void main(String[] args) {
+//        Course c = new Course(...); // Создаем полосу препятствий
+//        Team team = new Team(...); // Создаем команду
+//        c.doIt(team); // Просим команду пройти полосу
+//        team.showResults(); // Показываем результаты
+//        }
+
+public class MainTeam {
+    public static void main(String[] args) {
+        Team team = new Team();
+        team.start(); // инфра о команде и игре
+        Course obstacleCourse = new Course(); // Создаем полосу препятствий
+        String s= obstacleCourse.doIt(team); // Просим команду пройти полосу и получаем результаты
+        team.showResults(s); // Показываем результаты
+    }
 }
